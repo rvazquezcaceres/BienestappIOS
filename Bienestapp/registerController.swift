87f737d812933dedbb83fdeef1f3e2cbce5e9e6d
Fabregas <javier_fabregas_apps1ma1819@cev.com>
2020-01-16 10:11:21 +0100

    Created Login and Register view

---
+++ Bienestapp/registerController.swift
@@ -0,0 +1,16 @@
+//
+
+import Foundation
+
+import UIKit
+
+class registerController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
